45df8686385aa122a801ce67e9eb4d32f0b0ff25
Devin <devinmui@yahoo.com>
2016-03-25 22:44:35 -0700

    eigenfaces kinda figured out

--- eigen/faces.py
+++ eigen/faces.py
@@ -12,8 +12,11 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
+recognizer = cv2.createLBPHFaceRecognizer()
+
 for (x,y,w,h) in faces: # eigenfaces one of them
     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    break # lazy
 
 cv2.imshow('img',img)
 cv2.waitKey(0)
