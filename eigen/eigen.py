b86757b2e190078b7af20edb73cfa52e19edd38e
Devin <devinmui@yahoo.com>
2016-03-26 17:52:20 -0700

    fix something

--- eigen/eigen.py
+++ eigen/eigen.py
@@ -56,9 +56,9 @@ else:
 # mongo update
 '''
 
-db.users.update({"_id" : ObjectId("56f6fe5a1c733c6e79be1fc4")}, {"pictures": ["pictures/1.jpg", "pictures/6.jpg", "pictures/7.jpg", "pictures/8.jpg", "pictures/9.jpg", "pictures/10.jpg", "pictures/11.jpg"], "name": "Devin", "fb": "", "tw": "devinwmui"})
+db.users.update({"_id" : ObjectId("56f72c5aaeb085895a855f64")}, {"pictures": ["pictures/1.jpg", "pictures/6.jpg", "pictures/7.jpg", "pictures/8.jpg", "pictures/9.jpg", "pictures/10.jpg", "pictures/11.jpg"], "name": "Devin", "fb": "", "tw": "devinwmui"})
 
-db.users.update({"_id": ObjectId("56f70bf3ae8f15587b9108c1")}, {"pictures": ["pictures/2.jpg", "pictures/3.jpg", "pictures/4.jpg", "pictures/5.jpg"], "name": "Miguel", "tw": "pandawanyt", "fb": ""})
+db.users.update({"_id": ObjectId("56f72c70aeb085895a855f65")}, {"pictures": ["pictures/2.jpg", "pictures/3.jpg", "pictures/4.jpg", "pictures/5.jpg", "pictures/12.jpg", "pictures/13.jpg", "pictures/14.jpg"], "name": "Miguel", "tw": "pandawanyt", "fb": ""})
 
 
 '''
